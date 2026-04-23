a575f2f8fc6933832351578bc9002e8104501768
jiajunsun68 <2282875945@qq.com>
2026-04-23 21:26:46 +0800

    修复了一些显示bug

--- sw.js
+++ sw.js
@@ -24,7 +24,6 @@ const PRECACHE_LIST = [
   "./js/home.js",
   "./js/snackbar.js",
   "./img/icon_wechat.png",
-  "./img/avatar-hux.jpg",
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
